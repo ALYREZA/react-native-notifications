e7ca0f9040bad0758eddadc5bdaed50d5b7e4738
Alyreza <fanaticboy666@gmail.com>
2020-08-20 10:28:22 +0430

    polyfill proxy

--- lib/src/NotificationsIOS.ts
+++ lib/src/NotificationsIOS.ts
@@ -1,38 +1,42 @@
-import { Notification } from './DTO/Notification';
-import { Commands } from './commands/Commands';
-import { Platform } from 'react-native';
-import { EventsRegistryIOS } from './events/EventsRegistryIOS';
+import "proxy-polyfill";
+import { Notification } from "./DTO/Notification";
+import { Commands } from "./commands/Commands";
+import { Platform } from "react-native";
+import { EventsRegistryIOS } from "./events/EventsRegistryIOS";
 
 export class NotificationsIOS {
-  constructor(private readonly commands: Commands, private readonly eventsRegistry: EventsRegistryIOS) {
+  constructor(
+    private readonly commands: Commands,
+    private readonly eventsRegistry: EventsRegistryIOS
+  ) {
     return new Proxy(this, {
       get(target, name) {
-        if (Platform.OS === 'ios') {
+        if (Platform.OS === "ios") {
           return (target as any)[name];
         } else {
           return () => {};
         }
-      }
+      },
     });
   }
 
   /**
-  * Request permissions to send remote notifications
-  */
+   * Request permissions to send remote notifications
+   */
   public registerRemoteNotifications() {
     return this.commands.requestPermissions();
   }
 
   /**
-  * Unregister for all remote notifications received via Apple Push Notification service
-  */
+   * Unregister for all remote notifications received via Apple Push Notification service
+   */
   public abandonPermissions() {
     return this.commands.abandonPermissions();
   }
 
   /**
- * registerPushKit
- */
+   * registerPushKit
+   */
   public registerPushKit() {
     return this.commands.registerPushKit();
   }
